a27f75cbacd7434362b43983ee09930a4cf08a1e
samaramabbott <samara.mabbott@hotmail.com>
2026-05-15 20:15:31 +1000

    Update build.ts

--- script/build.ts
+++ script/build.ts
@@ -47,7 +47,7 @@ async function buildAll() {
   const externals = allDeps.filter((dep) => !allowlist.includes(dep));
 
   await esbuild({
-    entryPoints: ["server/index.ts"],
+    entryPoints: ["./server/index.ts"],
     platform: "node",
     bundle: true,
     format: "cjs",
